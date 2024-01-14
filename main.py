d942b7f341bd08548b873daca469d421ff66e4a0
Filip Michalski <118136393+pncqq@users.noreply.github.com>
2024-01-14 14:22:14 +0100

    Prawie gotowe.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pandas as pd
 from matplotlib import pyplot as plt
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
+
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+import seaborn as sns
+
 
 
 # Wczytywanie danych
@@ -60,7 +62,6 @@ plt.ylabel('Percentage of None/NaN Values')
 plt.xticks(range(len(none_frequency)), [str(i + 1) for i in range(len(none_frequency))], rotation=90)
 plt.show()
 
-
 # Usunięcie kolumny 'communityname'
 data = data.drop('communityname', axis=1)
 
@@ -73,13 +74,75 @@ missing_values = data.isna().sum()
 columns_to_drop = missing_values[missing_values > len(data) * threshold_for_missing_data].index
 data_cleaned = data.drop(columns=columns_to_drop)
 
+# Wypisz kolumny do wyrzucenia
+dropped_columns = columns_to_drop.tolist()
+print(f'Dropped columns: communityname, {dropped_columns}')
+print(f'Numbers of dropped columns: {len(dropped_columns) + 1}')
 
-# Usuwanie kolumny 'communityname'
+# # Usuwanie kolumny 'communityname'
 y = data_cleaned['ViolentCrimesPerPop']
 X = data_cleaned.drop('ViolentCrimesPerPop', axis=1)
 
 # Podział danych na zbiory treningowy i testowy
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
-wiersz_130= ['6','?','?','Watsonvillecity',1,0.03,0.78,0.01,0.31,0.34,1,0.46,0.57,0.36,0.39,0.05,1,0.27,0.53,0.34,0.36,0.54,0.59,0.5,0.25,0.18,0.26,0.22,0.16,0.31,0.19,0.23,0.04,0.4,1,0.83,0.15,0.75,0.46,0.36,0.27,0.61,0.19,0.36,0.48,0.5,0.46,0.9,0.55,0.53,0.68,0.69,0.57,0.5,0.02,0.37,0.07,0.41,0.5,0.52,0.54,1,1,1,0.98,0,1,1,1,0.82,0.57,1,0.24,1,0.82,0,0.02,0.83,0.35,0.26,0.51,0.67,0.2,0.59,0.47,0.48,0.48,0.46,0.53,0.62,0.54,0.72,0.83,0.25,0.07,0.06,1,0.46,0.41,0.72,0.73,'?','?','?','?','?','?','?','?','?','?','?','?','?','?','?','?','?',0.01,0.44,0.11,'?','?','?','?',0,'?',0.62]
-print(wiersz_130[31])
+# Obliczanie korelacji ze zmienna docelową
+feature_correlation = data_cleaned.corr()['ViolentCrimesPerPop'].sort_values()
+
+# Zakładamy, że "niska" korelacja oznacza wartość bezwzględną poniżej pewnego progu, np. 0.1
+low_correlation_threshold = 0.075
+low_corr_features = feature_correlation[abs(feature_correlation) < low_correlation_threshold]
+print("Cechy o niskiej korelacji z 'ViolentCrimesPerPop':\n", low_corr_features)
+
+low_corr_feature_names = low_corr_features.index.tolist()
+print("NAZWY Cechy o niskiej korelacji z 'ViolentCrimesPerPop':\n", low_corr_feature_names)
+
+
+# Trenowanie modelu losowego lasu
+rf = RandomForestRegressor(n_estimators=100, random_state=42)
+rf.fit(X_train, y_train)
+
+# Ważności cech
+feature_importances = pd.Series(rf.feature_importances_, index=X.columns).sort_values(ascending=False)
+
+# Zakładamy, że "niska" ważność oznacza wartość poniżej pewnego progu, np. 0.01
+low_importance_threshold = 0.01
+low_importance_features = feature_importances[feature_importances < low_importance_threshold]
+print("Cechy o niskiej ważności:\n", low_importance_features)
+
+
+unique_values_set = set(low_corr_features.index.tolist()) | set(low_importance_features.index.tolist())
+unique_values_series = pd.Series(list(unique_values_set))
+
+X_train_dropped = X_train.drop(unique_values_series, axis=1)
+X_test_dropped = X_test.drop(unique_values_series, axis=1)
+
+
+
+model = LinearRegression()
+model.fit(X_train_dropped, y_train)
+
+y_pred = model.predict(X_test_dropped)
+
+mse = mean_squared_error(X_test_dropped, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("Błąd średniokwadratowy (MSE):", mse)
+print("Współczynnik determinacji (R^2):", r2)
+
+
+
+
+
+
+
+# model = LinearRegression()
+# model.fit(X_train, y_train)
+#
+# y_pred = model.predict(X_test)
+#
+# mse = mean_squared_error(y_test, y_pred)
+# r2 = r2_score(y_test, y_pred)
+#
+# print("Błąd średniokwadratowy (MSE):", mse)
+# print("Współczynnik determinacji (R^2):", r2)
